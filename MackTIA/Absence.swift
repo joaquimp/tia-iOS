61b7406346038598bf60a2a4978238044a40163c
Aleph Retamal <retamal.ph@gmail.com>
2016-04-18 18:23:29 -0300

    AbsenceScene fetching data but not displaying all data. (search for TODO marks)

---
+++ MackTIA/Absence.swift
@@ -0,0 +1,23 @@
+//
+//  Absence.swift
+//  MackTIA
+//
+//  Created by Aleph Retamal on 4/18/16.
+//  Copyright © 2016 Mackenzie. All rights reserved.
+//
+
+import Foundation
+
+struct Absence {
+    // TODO: COLOCAR O RESTO DOS ATRIBUTOS
+    //    var atualizacao:String
+    //    var codigo:String
+    var dadas:Int
+    var disciplina:String
+    var faltas:Int
+    //    var nome_auno:String
+    //    var percentual:Int
+    //    var permit:Int
+    //    var permit20:Int
+    //    var turma:String
+}
---
+++ MackTIA/Scene/Absences/AbsenceTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  AbsenceTableViewCell.swift
+//  MackTIA
+//
+//  Created by Aleph Retamal on 4/18/16.
+//  Copyright © 2016 Mackenzie. All rights reserved.
+//
+
+import UIKit
+
+class AbsenceTableViewCell: UITableViewCell {
+
+    // TODO: COLOCAR O RESTO DOS OUTLETS
+    @IBOutlet weak var aulasPrevistasLabel: UILabel!
+    @IBOutlet weak var permitidasLabel: UILabel!
+    @IBOutlet weak var faltasLabel: UILabel!
+    @IBOutlet weak var nomeDaDisciplinaLabel: UILabel!
+
+}
